02848bf62962804f9da04924760e4ea9d0671c4a
daxtonhs <daxton.brouillette@student.doralacademynv.org>
2024-10-30 14:40:32 -0700

    nbnbn

--- day1/main.py
+++ day1/main.py
@@ -1,6 +1,7 @@
 number = 0
 with open("input.txt", "r") as file:
     for line in file:
-        temp = [for i in line]
-        print(temp)
+        temp = [i for i in line if i.isdigit()]
+        number += int(f"{temp[0]}{temp[-1]}")
 
+print(number)
